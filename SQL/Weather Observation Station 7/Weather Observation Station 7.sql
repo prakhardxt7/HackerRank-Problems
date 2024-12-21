9f8ddeb4e46a958761494e002d2c0dc1e1cbde37
Prakhar Dixit <54679515+prakhardxt7@users.noreply.github.com>
2024-12-21 16:00:46 +0530

    Create Weather Observation Station 7.sql

---
+++ SQL/Weather Observation Station 7/Weather Observation Station 7.sql
@@ -0,0 +1,9 @@
+-- MYSQL
+select distinct city from station where city like '%a'
+                            or city like '%e'
+                            or city like '%i'
+                            or city like '%o'
+                            or city like '%u';
+
+-- MSSQL Server 
+select distinct city from station where city like '%[aeiou]';
